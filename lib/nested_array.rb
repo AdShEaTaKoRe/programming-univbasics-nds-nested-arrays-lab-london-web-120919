a293ab47aa14ed01aa683424324ee165b1b39f30
Tommy Berman <tomy.berman1@gmail.com>
2019-12-03 15:08:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,8 +30,8 @@ def array_literal_matrix
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
   
-  conc = [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
-  conc.sort
+  conc = [ENNET_HOUSE.sort, ENFIELD_TENNIS_ACADEMY.sort]
+  
   
   
 end
